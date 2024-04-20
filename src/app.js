02fabe5b6eacb348218a1c912802321a837ebab9
JhojanBinary <102551448+JhojanBinary@users.noreply.github.com>
2024-04-20 16:08:05 +0000

    ExcuseV2

--- src/app.js
+++ src/app.js
@@ -5,7 +5,29 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+// Arrays con las partes de la excusa
+let who = ["El chancho", "Mi perro", "Su tortuga", "Mi pájaro"];
+let action = ["se comió", "mordió", "aplastó", "rompió"];
+let what = ["mi tarea", "la cama", "el coche"];
+let when = [
+  "antes de la clase",
+  "justo a tiempo",
+  "cuando terminé",
+  "durante mi almuerzo",
+  "mientras dormia"
+];
+//funcion que genera un numero aleatorio del tamaño de la lista
+//el valor string se almacena en "who" en formato de lista  y luego en randonWho para un mejor uso
+function generateExcuse() {
+  let randomWho = who[Math.floor(Math.random() * who.length)];
+  let randomAction = action[Math.floor(Math.random() * action.length)];
+  let randomWhat = what[Math.floor(Math.random() * what.length)];
+  let randomWhen = when[Math.floor(Math.random() * when.length)];
+
+  //suma  y concateancion de mis strings
+  let excusegenerator =
+    randomWho + " " + randomAction + " " + randomWhat + " " + randomWhen + ".";
+  return excusegenerator;
+}
+
+document.getElementById("excuse").innerText = generateExcuse();
